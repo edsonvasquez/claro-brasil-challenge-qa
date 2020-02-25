a683c8173834b805e692f9b9a5bd25d1d23eacb0
Edson Vasquez <edson.vasquez85@gmail.com>
2020-02-25 11:47:32 -0300

    Correcting some spelling errors in the specs - ios

--- mobile/ios/features/step_definitions/app_math_steps.rb
+++ mobile/ios/features/step_definitions/app_math_steps.rb
@@ -8,12 +8,12 @@ Given("I started Simple Calculator app") do
     puts "\nNumbers tested:\nFirst Number = #{@fst_number}\nSecond Number = #{@snd_number}\n"
   end
   
-  When("pressed at math operator {string}") do |op_id|
+  When("I pressed at math operator {string}") do |op_id|
     @base.click_element(ID["app"][op_id])
     @check_result = @app.check_operation(@fst_number, @snd_number, op_id)
   end
   
-  Then("the result have to be according to the math operation {string}") do |operation|
+  Then("the result has to be according to the math operation {string}") do |operation|
     calc_result = @app.result_return(ID["app"]["app_result"])
     expect(@check_result).to eql calc_result
     puts "\nOperation #{operation} results = #{calc_result}"
